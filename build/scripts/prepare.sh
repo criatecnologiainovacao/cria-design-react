c97392a1dd9580de046b144fc0977896e2e845b6
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-05-17 17:11:09 -0300

    melhoria

--- build/scripts/prepare.sh
+++ build/scripts/prepare.sh
@@ -8,13 +8,14 @@ if [ $? = 0 ]; then
   rm -fr dist
 
   # babel transform es6 into es5
-  babel build/npm/index.js --out-file dist/npm/es6/index.js
+  babel src --out-dir dist/npm/es5/src --copy-files
+  babel libs --out-dir dist/npm/es5/libs --copy-files
+  babel build/npm/index.js --out-file dist/npm/es5/index.js
 
   export BABEL_ENV=production
 
   babel src --out-dir dist/npm/es6/src --copy-files
   babel libs --out-dir dist/npm/es6/libs --copy-files
-  babel build/npm/index.js --out-file dist/npm/es6/index.js
 
   # keep es6 for next.js
   cp build/npm/index.js index.js
