33821de0ba603e8ef1a734a7d8bb8f0c06ff6871
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-05-20 10:05:21 -0300

    Commit message

--- build/scripts/prepare.sh
+++ build/scripts/prepare.sh
@@ -7,15 +7,10 @@ if [ $? = 0 ]; then
   # purge dist
   rm -fr dist
 
-  # babel transform es6 into es5
-  babel src --out-dir dist/npm/es5/src --copy-files
-  babel libs --out-dir dist/npm/es5/libs --copy-files
-  babel build/npm/index.js --out-file dist/npm/es5/index.js
-
   export BABEL_ENV=production
 
-  babel src --out-dir dist/npm/es6/src --copy-files
-  babel libs --out-dir dist/npm/es6/libs --copy-files
+  babel src --out-dir dist/npm/src --copy-files
+  babel libs --out-dir dist/npm/libs --copy-files
 
   # keep es6 for next.js
   cp build/npm/index.js index.js
