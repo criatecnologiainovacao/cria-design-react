945fedf1ba7b959051145263b90ae1fb868d8d7f
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-05-28 17:22:08 -0300

    corrigindo testes automatizados

--- src/components/step-bar/StepBar.test.jsx
+++ src/components/step-bar/StepBar.test.jsx
@@ -9,28 +9,25 @@ describe('StepBar test', () => {
 
     it('create', () => {
         const stepBar = shallow(<StepBar steps={steps}/>);
-        expect(stepBar.hasClass('cd-step-bar-content')).toBeTruthy();
-        expect(stepBar.childAt(0).hasClass('cd-step-bar')).toBeTruthy();
+        expect(stepBar.hasClass('cd-step-bar')).toBeTruthy();
     });
 
     it('clickable', () => {
         let activeStep = 0;
         const stepBar = shallow(<StepBar steps={steps} activeStep={activeStep} clickable/>);
-        expect(stepBar.hasClass('cd-step-bar-content')).toBeTruthy();
-        expect(stepBar.childAt(0).hasClass('cd-step-bar')).toBeTruthy();
-        expect(stepBar.childAt(0).childAt(0).hasClass('is-clickable')).toBeTruthy();
+        expect(stepBar.hasClass('cd-step-bar')).toBeTruthy();
+        expect(stepBar.childAt(0).hasClass('is-clickable')).toBeTruthy();
     });
 
     it('onClick', () => {
         const onClick = sinon.spy();
-        let activeStep = 1;
+        let activeStep = 0;
         const stepBar = shallow(<StepBar steps={steps} activeStep={activeStep} onClick={onClick} clickable/>);
 
         stepBar.find('.cd-step-bar').find('.is-clickable').simulate('click');
 
         expect(onClick.calledOnce).toBe(true);
-        expect(stepBar.hasClass('cd-step-bar-content')).toBeTruthy();
-        expect(stepBar.childAt(0).hasClass('cd-step-bar')).toBeTruthy();
-        expect(stepBar.childAt(0).childAt(0).hasClass('is-clickable')).toBeTruthy();
+        expect(stepBar.hasClass('cd-step-bar')).toBeTruthy();
+        expect(stepBar.childAt(0).hasClass('is-clickable')).toBeTruthy();
     });
 });
