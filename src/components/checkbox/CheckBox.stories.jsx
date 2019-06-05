df7bd99820b53d96fedafa82a07cdf1d8895dd17
Highlander Paiva <highlanderpaiva.lemaf@gmail.com>
2019-06-05 06:59:30 -0300

    refactor(checkbox): adicionando testes para o checkbox e corrigindo estilo

--- src/components/checkbox/CheckBox.stories.jsx
+++ src/components/checkbox/CheckBox.stories.jsx
@@ -5,14 +5,14 @@ import Checkbox from './CheckBox';
 
 const groupState = {
     checkList: ['Option A', 'Selected and disabled']
-}
+};
 
 const buttonState = {
     cities: ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen'],
     checkboxGroup1: ['Shanghai'],
     checkboxGroup2: ['Beijing'],
     checkboxGroup3: ['Guangzhou']
-}
+};
 
 storiesOf('CheckBox', module)
     .add('default', () => {
@@ -29,46 +29,110 @@ storiesOf('CheckBox', module)
                 <Checkbox checked disabled>Option 2</Checkbox>
             </div>
         )
-      })
+    })
+    .add('boolean labels', () => {
+        return (
+            <div>
+                <Checkbox
+                    trueLabel="Checked"
+                    falseLabel="Unchecked"
+                />
+            </div>
+        )
+    })
     .add('group', () => {
         return (
             <Checkbox.Group value={groupState.checkList}>
-                <Checkbox label="Option A"></Checkbox>
-                <Checkbox label="Option B"></Checkbox>
-                <Checkbox label="Option C"></Checkbox>
-                <Checkbox label="Disabled" disabled></Checkbox>
-                <Checkbox label="Selected and disabled" disabled></Checkbox>
+                <Checkbox label="Option A"/>
+                <Checkbox label="Option B"/>
+                <Checkbox label="Option C"/>
+                <Checkbox label="Disabled" disabled/>
+                <Checkbox label="Selected and disabled" disabled/>
+            </Checkbox.Group>
+        )
+    })
+    .add('max', () => {
+        return (
+            <Checkbox.Group max={2}>
+                <Checkbox label="Option A"/>
+                <Checkbox label="Option B"/>
+                <Checkbox label="Option C"/>
+                <Checkbox label="Disabled" disabled/>
+                <Checkbox label="Selected and disabled" disabled/>
+            </Checkbox.Group>
+        )
+    })
+    .add('min', () => {
+        return (
+            <Checkbox.Group max={2}>
+                <Checkbox label="Option A" checked/>
+                <Checkbox label="Option B" checked/>
+                <Checkbox label="Option C"/>
+                <Checkbox label="Disabled" disabled/>
+                <Checkbox label="Selected and disabled" disabled/>
             </Checkbox.Group>
         )
     })
     .add('button style', () => {
         return (
             <div>
-                <div style={{margin: '15px 0'}}></div>
+                <div style={{ margin: '15px 0' }}/>
                 <Checkbox.Group value={buttonState.checkboxGroup1}>
                     {
                         buttonState.cities.map((city, index) => {
-                        return <Checkbox.Button key={index} label={city}>{city}</Checkbox.Button>
-                    })
+                            return (
+                                <Checkbox.Button
+                                    key={index}
+                                    label={city}
+                                >
+                                    {city}
+                                </Checkbox.Button>
+                            )
+                        })
                     }
                 </Checkbox.Group>
-                <div style={{margin: '15px 0'}}></div>
-                <Checkbox.Group value={buttonState.checkboxGroup2} size="small">
+                <div style={{ margin: '15px 0' }}/>
+                <Checkbox.Group
+                    value={buttonState.checkboxGroup2}
+                    size="small"
+                >
                     {
                         buttonState.cities.map((city, index) => {
-                        return <Checkbox.Button key={index} label={city} disabled={city === 'Shenzhen'}>{city}</Checkbox.Button>
-                    })
+                            return (
+                                <Checkbox.Button
+                                    key={index}
+                                    label={city}
+                                    disabled={city === 'Shenzhen'}
+                                >
+                                    {city}
+                                </Checkbox.Button>
+                            )
+                        })
                     }
                 </Checkbox.Group>
-                <div style={{margin: '15px 0'}}></div>
-                <Checkbox.Group value={buttonState.checkboxGroup3} size="large" fill="#324057" textColor="#a4aebd" min="1" max="3">
+                <div style={{ margin: '15px 0' }}/>
+                <Checkbox.Group
+                    value={buttonState.checkboxGroup3}
+                    size="large"
+                    fill="#324057"
+                    textColor="#a4aebd"
+                    min="1"
+                    max="3"
+                >
                     {
                         buttonState.cities.map((city, index) => {
-                        return <Checkbox.Button key={index} label={city}>{city}</Checkbox.Button>
-                    })
+                            return (
+                                <Checkbox.Button
+                                    key={index}
+                                    label={city}
+                                >
+                                    {city}
+                                </Checkbox.Button>
+                            )
+                        })
                     }
                 </Checkbox.Group>
             </div>
         )
     });
-    
+
