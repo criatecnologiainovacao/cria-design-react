9f0f47baa108d2d366876992bb33461e1e9dd97c
Claudio V. de Carvalho <claudio.carvalho@lemaf.ufla.br>
2019-06-19 18:40:08 -0300

    Iniciando testes do componente de Notification

--- src/components/notification/Notification.test.jsx
+++ src/components/notification/Notification.test.jsx
@@ -1,25 +1,86 @@
 import React from 'react';
-import { mount } from 'enzyme';
 import sinon from 'sinon';
 
-import Popover from '.';
-import Button from '../button';
+import Notification from '.';
 
 describe('Notification test', () => {
-    it('Deve renderizar um popover simples', () => {
+    beforeEach(() => {
+      const el = document.querySelector('.cd-notification');
+      if (!el) return;
+      if (el.parentNode) {
+        el.parentNode.removeChild(el);
+      }
+    });
 
-        const component = mount(
-            <div>
-                <Popover placement="top-start" title="My title"
-                         width="200" visible={true}
-                         content="Conteúdo do popover">
-                    <Button>Hover to activate</Button>
-                </Popover>
-            </div>
-        );
+    it('Deve fechar automaticamente', () => {
+        Notification({
+            title: 'Title',
+            message: 'This is a reminder',
+            duration: 500
+        });
 
-        expect(component.find('.cd-popover').exists()).toBeTruthy();
-        expect(component.find('.cd-popover__title').text()).toEqual('My title');
+        expect(document.querySelector('.cd-notification')).not.toBeNull();
 
+        setTimeout(() => {
+            expect(document.querySelector('.cd-notification')).toBeNull();
+        }, 1000);
+    });
+
+    it('Deve fechar manualmente', () => {
+        const onClose = sinon.spy();
+        const onClick = sinon.spy();
+
+        Notification({
+            title: 'Title',
+            message: 'This is a reminder',
+            onClose,
+            onClick
+        });
+
+        const closeButton = document.querySelector('.cd-notification__closeBtn');
+        
+        expect(closeButton).not.toBeNull();
+
+        closeButton.dispatchEvent(new Event('click'));
+
+        setTimeout(() => {
+            expect(document.querySelector('.cd-notification')).toBeNull();
+        }, 1000);
+    });
+
+    it('Deve permitir definir um tipo', () => {
+        Notification({
+            title: 'Title',
+            message: 'This is a reminder',
+            type: 'success'
+        });
+
+        const notification = document.querySelector('.cd-notification');
+        
+        expect(notification).not.toBeNull();
+
+        expect(notification.className.split(" ").indexOf("success")).toBeGreaterThan(-1);
+    });
+
+    it('Deve possuir eventos', () => {
+        const onClose = sinon.spy();
+        const onClick = sinon.spy();
+
+        Notification({
+            title: 'Title',
+            message: 'This is a reminder',
+            onClose,
+            onClick
+        });
+
+        const closeButton = document.querySelector('.cd-notification__closeBtn');
+        
+        expect(closeButton).not.toBeNull();
+
+        closeButton.dispatchEvent(new Event('click'));
+
+        setTimeout(() => {
+            expect(document.querySelector('.cd-notification')).toBeNull();
+        }, 1000);
     });
 });
