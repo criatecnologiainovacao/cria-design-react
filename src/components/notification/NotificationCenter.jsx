065a570af06f37dbe80b3352242c9891743f84d6
Highlander Paiva <highlanderpaiva.lemaf@gmail.com>
2019-07-11 10:57:58 -0300

    fix: corrigindo code smells

--- src/components/notification/NotificationCenter.jsx
+++ src/components/notification/NotificationCenter.jsx
@@ -5,7 +5,7 @@ import Notification from './Notification';
 
 const className = '.cd-notification';
 
-export default function NotificationCenter(props = {}, type) {
+export default function NotificationCenter(type, props = {}) {
   const div = document.createElement('div');
 
   document.body.appendChild(div);
@@ -24,7 +24,7 @@ export default function NotificationCenter(props = {}, type) {
     props.offset = 0
   }
 
-  const instances = document.querySelectorAll(className)
+    const instances = document.querySelectorAll(className);
 
   const lastInstance = instances[instances.length - 1];
 
@@ -34,13 +34,13 @@ export default function NotificationCenter(props = {}, type) {
     willUnmount(height, top) {
       setTimeout(() => document.body.removeChild(div));
       requestAnimationFrame(() => {
-        const instances = document.querySelectorAll(className);
-        const len = instances.length;
+          const inst = document.querySelectorAll(className);
+          const len = inst.length;
         for (let i = 0; i < len; i++) {
-          const element = instances[i];
-          const elementTop = parseInt(element.style.top);
+            const elem = inst[i];
+            const elementTop = parseInt(elem.style.top);
           if (elementTop > top) {
-            element.style.top = `${elementTop - height - 16}px`;
+              elem.style.top = `${elementTop - height - 16}px`;
           }
         }
       })
@@ -52,6 +52,6 @@ export default function NotificationCenter(props = {}, type) {
 
 /* eslint-disable */
 ['success', 'warning', 'info', 'error'].forEach(type => {
-  NotificationCenter[type] = (options = {}) => NotificationCenter(options, type);
+    NotificationCenter[type] = (options = {}) => NotificationCenter(type, options);
 });
 /* eslint-enable */
